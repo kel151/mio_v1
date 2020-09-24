0b6204999acd05d13e11ba4ea48d3326f84ea711
kel151 <kelvin.dafiemubo3@mail.dcu.ie>
2020-09-24 03:47:13 +0000

    added sorting and product count to collection

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    categories = None
+    categories = Category.objects.all()
     sort = None
     direction = None
 
@@ -21,7 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
