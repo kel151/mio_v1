bae54882930f2e82e5c55e2ebe83660397910a3e
kel151 <kelvin.dafiemubo3@mail.dcu.ie>
2020-10-03 01:38:50 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HXzJ4HBU1PUzPaEyzSwHxXGA4CBuGTdF6xC3wDW0C0eqm7deq6C8DxoR3IhPVDbcfYk7tjO0xeV8rCSiwX0kous00GA0N4pGw',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
